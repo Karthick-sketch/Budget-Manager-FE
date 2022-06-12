98b304a530b85f1e629aeada8a3b2f2b6023c711
Karthick-Ramesh-16 <karthick.ramesh@freshworks.com>
2022-06-12 13:41:38 +0530

    Initial commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'budget-manager-fe/app';
+import config from 'budget-manager-fe/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
