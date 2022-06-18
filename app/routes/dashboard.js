d7c0c18fe63f69e3e45e552887c96b5c75c5c69d
Karthick-Ramesh-16 <karthick.ramesh@freshworks.com>
2022-06-18 14:27:47 +0530

    Print sign symbols in dynamically

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -4,6 +4,21 @@ export default class DashboardRoute extends Route {
   async model() {
     let response = await fetch('/dashboard.json');
     let { data } = await response.json();
+
+    for (let d of data) {
+      switch (d['type']) {
+        case 'expenses':
+          d['amount'] = `-₹${d['amount']}`;
+          break;
+        case 'incomes':
+          d['amount'] = `+₹${d['amount']}`;
+          break;
+        case 'balance':
+          d['amount'] = (d['amount'] < 0 ? '-₹' : '+₹') + Math.abs(d['amount']);
+          break;
+      }
+    }
+
     return data;
   }
 }
